8974a6dbac0514c1afc86338798faea9018a652f
Michael Kam <michael.kam@ubc.ca>
2014-03-12 11:50:29 -0700

    Co-authors plus: remove author box when co-authors support, and add co-authors box for users who can create multiple profiles

--- lib/class.profile-cct-manage-table.php
+++ lib/class.profile-cct-manage-table.php
@@ -7,8 +7,24 @@ class Profile_CCT_Table {
 		
 		global $coauthors_plus;
 		if ( class_exists('coauthors_plus') && isset($coauthors_plus) ) {
-			add_filter ('manage_edit-profile_cct_columns',		array( $coauthors_plus, '_filter_manage_posts_columns') );
+			// Show multiple authors in dashboard profile listing
+			add_filter ('manage_edit-profile_cct_columns',	array( $coauthors_plus, '_filter_manage_posts_columns') );
+			
+			// Hide default author box
+			add_filter ('add_meta_boxes_profile_cct', 		array( __CLASS__, 'remove_authors_box'), 11 );
+			// Add co-author box to users who can create multiple profiles or manage all profiles
+			add_action ( 'coauthors_plus_edit_authors', 	array( __CLASS__, 'coauthors_plus_edit_authors') );
 		}
+	}
+	
+	function coauthors_plus_edit_authors ($post_types) {
+		// Added co-authors box to users who can "Create multiple profiles" (OR "Manage all profiles")
+		//return current_user_can('edit_profiles_cct') || current_user_can('edit_others_profile_cct');
+		return current_user_can('edit_profiles_cct');
+	}
+	
+	function remove_authors_box() {
+		remove_meta_box ( 'authordiv', 'profile_cct', 'side' );
 	}
 	
 	function register( $columns ) {
